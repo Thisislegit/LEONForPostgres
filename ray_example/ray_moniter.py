39065469ec1fbccca0ddc1b33c2ef95f3f55dba9
thisislegit <71903722@qq.com>
2023-11-28 20:22:03 +0800

    ray example for check complete task

--- ray_example/ray_moniter.py
+++ ray_example/ray_moniter.py
@@ -5,9 +5,10 @@ import json
 from ray import cluster_utils
 from ray_test import FileWriter
 
-address='121.48.161.203:7777'
+address='121.48.161.203:44116'
 context = ray.init(address, namespace='ray_namespace')
 
-actor = FileWriter.options(name="foo", lifetime="detached", get_if_exists=True).remote("file.txt")
+actor = ray.get_actor('foo')
+# actor = FileWriter.options(name="foo", lifetime="detached", get_if_exists=True).remote("file.txt")
 print('New task submitted, waiting for completion...')
-print(ray.get(actor.complete_all_tasks.remote(10)))
+print(ray.get(actor.complete_all_tasks.remote()))
--- ray_example/ray_test.py
+++ ray_example/ray_test.py
@@ -9,28 +9,33 @@ class FileWriter:
     def __init__(self, file_path):
         self.file_path = file_path
         self.completed_tasks = 0
+        self.send_task = 0
 
     def write_file(self):
         with open(self.file_path, 'ab') as file:
-            time.sleep(10)
+            time.sleep(2)
             file.write(b'hello world\n')
         self.completed_tasks += 1
         print(1111111)
     
-    def complete_all_tasks(self, task_num):
-        print(self.completed_tasks)
-        if self.completed_tasks == task_num:
+    def Add_task(self):
+        self.send_task += 1
+    
+    def complete_all_tasks(self):
+        print("Complete:", self.completed_tasks)
+        if self.completed_tasks == self.send_task:
             return True
         else:
             return False
 
 if __name__ == "__main__":
-    address='121.48.161.203:7777'
-    context = ray.init(address, namespace='ray_namespace')
+    # address='121.48.161.203:7777'
+    context = ray.init(namespace='ray_namespace')
     print(context.address_info)
     file_path = "./file.txt"
     writer = FileWriter.options(name="foo", lifetime="detached").remote(file_path)
-    for _ in range(10):
+    for _ in range(1):
+        ray.get(writer.Add_task.remote())
         object_id = writer.write_file.remote()
     # keep this program alive
     while True:
