612e259fee4910abf1e742b63b3e82b84424d8d5
Thisislegit <71903722@qq.com>
2023-12-27 05:59:16 +0000

    fix for online training

--- leon_pl_v1.py
+++ leon_pl_v1.py
@@ -29,7 +29,6 @@ from argparse import ArgumentParser
 import copy
 import wandb
 import time
-import random
 from tqdm import tqdm
 from config import read_config
 
@@ -256,21 +255,8 @@ if __name__ == '__main__':
     dict_actor = ray.get_actor('querydict')
     actors = [ActorThatQueries.options(name=f"actor{port}").remote(port) for port in ports]
     pool = ActorPool(actors)
-    # training_query = load_training_query("./train/training_query/job.txt")
-    # train_files = [i[0] for i in training_query]
-    train_files = ['1a', '1b', '1c', '1d', '2a', '2b', '2c', '2d', '3a', '3b', '3c', '4a',
-                    '4b', '4c', '5a', '5b', '5c', '6a', '6b', '6c', '6d', '6e', '6f', '7a', 
-                    '7b', '7c', '8a', '8b', '8c', '8d', '9a', '9b', '9c', '9d', '10a', '10b', 
-                    '10c', '11a', '11b', '11c', '11d', '12a', '12b', '12c', '13a', '13b', '13c', 
-                    '13d', '14a', '14b', '14c', '15a', '15b', '15c', '15d', '16a', '16b', '16c',
-                    '16d', '17a', '17b', '17c', '17d', '17e', '17f', '18a', '18b', '18c', '19a',
-                    '19b', '19c', '19d', '20a', '20b', '20c', '21a', '21b', '21c', '22a', '22b',
-                    '22c', '22d', '23a', '23b', '23c', '24a', '24b', '25a', '25b', '25c', '26a', 
-                    '26b', '26c', '27a', '27b', '27c', '28a', '28b', '28c', '29a', '29b', '29c',
-                    '30a', '30b', '30c', '31a', '31b', '31c', '32a', '32b', '33a', '33b', '33c']
-    random.shuffle(train_files)
+    train_files, training_query = envs.load_train_files(conf['leon']['workload_type'])
     ray.get(dict_actor.write_sql_id.remote(train_files))
-    train_files = train_files * 75
     chunk_size = 5 # the # of sqls in a chunk
     IF_TRAIN = True
     model_path = "./log/model.pth"
@@ -308,8 +294,8 @@ if __name__ == '__main__':
     ch_start_idx = 0 # the start idx of the current chunk in train_files
     while ch_start_idx + chunk_size <= len(train_files):
         print(f"\n+++++++++ a chunk of sql from {ch_start_idx}  ++++++++")
-        sqls_chunk = load_sql(train_files[ch_start_idx : ch_start_idx + chunk_size])
-        # sqls_chunk = load_sql(list(range(ch_start_idx, ch_start_idx + chunk_size)), training_query=training_query)
+        # sqls_chunk = load_sql(train_files[ch_start_idx : ch_start_idx + chunk_size])
+        sqls_chunk = load_sql(list(range(ch_start_idx, ch_start_idx + chunk_size)), training_query=training_query)
         curr_file = train_files[ch_start_idx : ch_start_idx + chunk_size]
         print(train_files[ch_start_idx : ch_start_idx + chunk_size])
         time_ratio = []
