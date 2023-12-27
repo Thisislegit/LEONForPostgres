490adde764f38da6c570fd1b65fe8b27ab437b22
Thisislegit <71903722@qq.com>
2023-12-27 05:59:16 +0000

    fix for online training

--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Check if ./log/ exists, if not create it
+if [ ! -d "./log/" ]; then
+    mkdir "./log/"
+fi
+
+# Check if ./logs/ exists, if not create it
+if [ ! -d "./logs/" ]; then
+    mkdir "./logs/"
+fi
+
 # 进入 log 目录并删除 model.pth 和 message.pkl
 cd log
 rm model.pth
